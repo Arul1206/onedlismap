e8a14eb553bd268694673ba95e87d6a7b5f21ab4
Arul1206 <kumararrul@gmail.com>
2019-08-27 10:01:07 +0530

    cucumber file

---
+++ src/test/java/org/tcs/Class1.java
@@ -0,0 +1,47 @@
+package org.tcs;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class Class1 {
+	static WebDriver driver;
+	@Given("user should launch browser")
+	public void user_should_launch_browser() {
+		System.setProperty("webdriver.chrome.driver","H:\\Arul\\SelCucumber\\Driver\\chromedriver.exe");
+	      driver=new ChromeDriver();
+	    driver.get("http://demo.guru99.com/telecom/");
+	}
+
+	@Given("user click add customer link")
+	public void user_click_add_customer_link() {
+		driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();    
+	}
+	/*@When("user will provide vaild details")
+	public void user_will_provide_vaild_details() {
+		driver.findElement(By.xpath("//label[text()='Done']")).click();  
+		driver.findElement(By.id("fname")).sendKeys("vishal");
+		driver.findElement(By.id("lname")).sendKeys("Raj");
+		driver.findElement(By.id("email")).sendKeys("raj@gmail.com");
+		driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys("Chennai");
+		driver.findElement(By.id("telephoneno")).sendKeys("9865182316");
+		driver.findElement(By.xpath("//input[@name='submit']")).click();  
+	}*/
+	@When("user will provide vaild details{string},{string},{string},{string},{string}")
+	public void user_will_provide_vaild_details(String fname, String lname, String email, String address, String phone) {	driver.findElement(By.xpath("//label[text()='Done']")).click();  
+		driver.findElement(By.id("fname")).sendKeys(fname);
+		driver.findElement(By.id("lname")).sendKeys(lname);
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(address);
+		driver.findElement(By.id("telephoneno")).sendKeys(phone);
+		driver.findElement(By.xpath("//input[@name='submit']")).click();
+	}
+	@Then("to verified the customer id is displayed")
+	public void to_verified_the_customer_id_is_displayed() {
+		Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
+	}
+
+	}
---
+++ target/classes/Feature/try.feature
@@ -0,0 +1,10 @@
+
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: Generate customer id
+    Given user should launch the browser
+    And user add customer link
+    When user provide valid details
+    Then to verify the customer id is displayed
